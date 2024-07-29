359dcb7db685737c8504145224c171c3219b85a6
Jorgearaya47 <jorgearaya474@gmail.com>
2024-07-28 21:12:34 -0600

    add code autocompletion

--- admin/settings.php
+++ admin/settings.php
@@ -18,21 +18,21 @@ add_action( 'admin_menu', 'additional_scss_menu' );
 
 // Plugin setttings page
 function additional_scss_page() {
+    $value = get_option('scss_additional_code');
     ?>
     <div class="wrap">
-        <h1><?php esc_html_e('Additional SCSS', 'additional-scss'); ?></h1>
+        <h1>Additional SCSS</h1>
         <form method="post" action="options.php">
             <?php
             settings_fields('additional_scss_settings');
             do_settings_sections('additional-scss');
             ?>
-            <textarea id="scss_additional_code" name="scss_additional_code" rows="10" cols="50"><?php echo esc_textarea(get_option('scss_additional_code')); ?></textarea>
+            <textarea id="scss_additional_code" name="scss_additional_code" rows="10" cols="50"><?php echo stripslashes( $value ); ?></textarea>
             <?php submit_button(); ?>
         </form>
     </div>
     <?php
 }
-add_action( 'admin_init', 'additional_scss_page' );
 
 // Register settings
 function additional_scss_settings() {
@@ -46,9 +46,17 @@ function enqueue_codemirror_assets($hook) {
         return;
     }
 
-    wp_enqueue_style( 'codemirror', plugins_url( 'assets/codemirror/lib/codemirror.css', __FILE__ ) );
-    wp_enqueue_script( 'codemirror', plugins_url( 'assets/codemirror/lib/codemirror.js', __FILE__ ), array(), null, true );
-    wp_enqueue_script( 'codemirror-css', plugins_url( 'assets/codemirror/mode/css/css.js', __FILE__ ), array( 'codemirror' ), null, true );
-    wp_enqueue_script( 'codemirror-sass', plugins_url( 'assets/codemirror/mode/sass/sass.js', __FILE__ ), array( 'codemirror' ), null, true );
-    wp_enqueue_script( 'additional-scss-codemirror', plugins_url( 'assets/js/additional-scss-codemirror.js', __FILE__ ), array( 'codemirror' ), null, true );
+    wp_enqueue_style( 'codemirror', PLUGIN_PATH  . 'assets/codemirror/lib/codemirror.css');
+    wp_enqueue_script( 'codemirror', PLUGIN_PATH . 'assets/codemirror/lib/codemirror.js', array(), null, true );
+    wp_enqueue_script( 'codemirror-css', PLUGIN_PATH  . 'assets/codemirror/mode/css/css.js', array( 'codemirror' ), null, true );
+    wp_enqueue_script( 'codemirror-sass', PLUGIN_PATH . 'assets/codemirror/mode/sass/sass.js', array( 'codemirror' ), null, true );
+
+    // Codemirror addons
+    wp_enqueue_script('codemirror-addon-closebrackets', PLUGIN_PATH . 'assets/codemirror/addon/edit/closebrackets.js', array('codemirror'), null, true);
+    wp_enqueue_script('codemirror-addon-show-hint', PLUGIN_PATH . 'assets/codemirror/addon/hint/show-hint.js', array('codemirror'), null, true);
+    wp_enqueue_style('codemirror-addon-show-hint', PLUGIN_PATH . 'assets/codemirror/addon/hint/show-hint.css');
+
+    // Editor settings
+    wp_enqueue_script( 'additional-scss-codemirror', PLUGIN_PATH . 'assets/js/additional-scss-codemirror.js', array( 'codemirror' ), null, true );
 }
+add_action('admin_enqueue_scripts', 'enqueue_codemirror_assets');
